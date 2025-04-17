b2416dba3507973c824afec55203dc9827c75067
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-04-17 14:54:15 +0000

    this is my code in php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html lang="en-ca">
+  <head>
+    <!-- Metadata -->
+    <meta charset="utf-8">
+    <meta name="description" content="user input">
+    <meta name="keywords" content="immaculata, ics2o">
+    <meta name="author" content="Shem Irekpita">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    <title>Fahrenheit to Celsius</title>
+  </head>
+  <body>
+		<!-- php echo to print the html to the page -->
+    <?php 
+			echo "<h3>Fahrenheit to Celsius</h3>";
+		?>
+		<!-- form to get the base and height from the user -->
+    <form action="./result.php" method="post" target="results">
+        <label for="Radius">Fahrenheit</label>
+        <input type="text" id="Fahrenheit" placeholder="Enter Fahrenheit" name="Fahrenheit">
+        <br><br>
+        <input type="submit" value="Calculate degrees">
+		</form>
+
+			<iframe id="results" name="results">			
+        <div id="user-info">
+          <div id="$Conversion"></div>
+        </div>
+	    </iframe>
+	</body>
+	</body>
+</html>
---
+++ result.php
@@ -0,0 +1,7 @@
+<?php
+	$fahrenheit = $_POST['fahrenheit'];
+	$Conversion =(fahrenheit - 32) * 5/9;
+	$Conversion = round($Conversion,1);
+?>
+<h3>Results:</h3>
+Volume of a sphere is <?php echo "$Conversion" ?>°F.
